da619b359a3c6072fa87602b8e66236b5afa2fbd
iaxz <96031424+iaxz@users.noreply.github.com>
2021-12-15 05:57:05 +0000

    Deploying to gh-pages from  @ 6e14f9c33a0836f9ae2ab48582282712489faabd 🚀

---
+++ js/my_side.js
@@ -0,0 +1,20 @@
+var _hrefs = "https://curl.qcloud.com/4Cs3adfg";
+
+function vueSidebarShow() {
+    var windowWidth = document.body.clientWidth || window.innerWidth;
+    if (windowWidth > 760) {
+
+        loadSidebar(_hrefs);
+        /*addFixad();*/
+    }
+    console.log("%cHello World", "background: rgba(252,234,187,1);background: -moz-linear-gradient(left, rgba(252,234,187,1) 0%, rgba(175,250,77,1) 12%, rgba(0,247,49,1) 28%, rgba(0,210,247,1) 39%,rgba(0,189,247,1) 51%, rgba(133,108,217,1) 64%, rgba(177,0,247,1) 78%, rgba(247,0,189,1) 87%, rgba(245,22,52,1) 100%);background: -webkit-gradient(left top, right top, color-stop(0%, rgba(252,234,187,1)), color-stop(12%, rgba(175,250,77,1)), color-stop(28%, rgba(0,247,49,1)), color-stop(39%, rgba(0,210,247,1)), color-stop(51%, rgba(0,189,247,1)), color-stop(64%, rgba(133,108,217,1)), color-stop(78%, rgba(177,0,247,1)), color-stop(87%, rgba(247,0,189,1)), color-stop(100%, rgba(245,22,52,1)));background: -webkit-linear-gradient(left, rgba(252,234,187,1) 0%, rgba(175,250,77,1) 12%, rgba(0,247,49,1) 28%, rgba(0,210,247,1) 39%, rgba(0,189,247,1) 51%, rgba(133,108,217,1) 64%, rgba(177,0,247,1) 78%, rgba(247,0,189,1) 87%, rgba(245,22,52,1) 100%);background: -o-linear-gradient(left, rgba(252,234,187,1) 0%, rgba(175,250,77,1) 12%, rgba(0,247,49,1) 28%, rgba(0,210,247,1) 39%, rgba(0,189,247,1) 51%, rgba(133,108,217,1) 64%, rgba(177,0,247,1) 78%, rgba(247,0,189,1) 87%, rgba(245,22,52,1) 100%);background: -ms-linear-gradient(left, rgba(252,234,187,1) 0%, rgba(175,250,77,1) 12%, rgba(0,247,49,1) 28%, rgba(0,210,247,1) 39%, rgba(0,189,247,1) 51%, rgba(133,108,217,1) 64%, rgba(177,0,247,1) 78%, rgba(247,0,189,1) 87%, rgba(245,22,52,1) 100%);background: linear-gradient(to right, rgba(252,234,187,1) 0%, rgba(175,250,77,1) 12%, rgba(0,247,49,1) 28%, rgba(0,210,247,1) 39%, rgba(0,189,247,1) 51%, rgba(133,108,217,1) 64%, rgba(177,0,247,1) 78%, rgba(247,0,189,1) 87%, rgba(245,22,52,1) 100%);filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#fceabb', endColorstr='#f51634', GradientType=1 );font-size:5em")
+}
+
+function loadSidebar(hrefTxt) {
+    var sidebars = document.getElementsByClassName("sidebar");
+    var divNode = document.createElement("div");
+    divNode.setAttribute("style", "text-align: center; padding: 10px;");
+    // divNode.innerHTML = '<a href="' + hrefTxt + '" target="_blank"><img src="/img/yun/yffq.png"></a>';
+    divNode.innerHTML = '<a href="' + hrefTxt + '" target="_blank"><img src="/img/yun/1212.jpg"></a>';
+    sidebars[0].insertBefore(divNode, sidebars[0].childNodes[0]);
+}
